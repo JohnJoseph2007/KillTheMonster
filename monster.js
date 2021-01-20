bac8d56f04bfdaa0c86d90606c6c1bfac6a88434
John Joseph <47251026+JohnJoseph2007@users.noreply.github.com>
2021-01-20 12:59:48 +0530

    Update monster.js

--- monster.js
+++ monster.js
@@ -1,7 +1,7 @@
 class Monster {
     constructor() {
         var options = {restitution:0.5, friction:0.3, isStatic:false};
-        this.body = b.rectangle(675, 500, 80, 80);
+        this.body = b.rectangle(900, 500,160, 160);
         this.image = loadImage("images/Monster-01.png");
         world.add(wo, this.body);
     }
@@ -10,6 +10,6 @@ class Monster {
         var pos =this.body.position;
 
         imageMode(CENTER);
-        image(pos.x, pos.y, 80, 80);
+        image(this.image, pos.x, pos.y, 160, 160);
     }
 }
